830b09d2fa9185691983947eec69a40828af3555
jjtheall <jtheall@quinnipiac.edu>
2021-10-06 16:12:22 -0400

    added instance vars for insulin carb ratio, correction factor, and target blood glucose

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,11 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private int ICRatio;
+    private int correctionFactor;
+    private int targetBG;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Omnipod Dash");
+        primaryStage.setScene(new Scene(root, 450, 550));
         primaryStage.show();
     }
 
